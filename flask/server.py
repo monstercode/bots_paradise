75ef90047b6bd34efbc54f3ff2bebe9f80205945
monstercode <javiersm00@gmail.com>
2023-02-06 01:22:42 -0300

    add file upload handling

--- flask/server.py
+++ flask/server.py
@@ -4,9 +4,18 @@ from datetime import datetime
 from flask import Flask, request
 from pymongo import MongoClient
 import logging
+import sys
+import uuid
+import pathlib
+
+logging.StreamHandler(sys.stdout)
 logger = logging.getLogger("main")
+logger.setLevel(logging.DEBUG)
+
 
 app = Flask(__name__)
+pathlib.Path("/src/uploads").mkdir(exist_ok=True)
+app.config['UPLOAD_FOLDER'] = "/src/uploads"
 
 client = MongoClient("mongo:27017")
 
@@ -26,7 +35,7 @@ def bots_handler(path):
             "datetime": datetime.utcnow(),
             "uri": request.path,
             "method": request.method,
-            "request_data": request.data or request.form,
+            "request_data": (request.data or request.form) if not request.files else f"<uploaded file(s)>: {len(request.files.keys())}",
             "query_string": dict(request.args),
             "origin": request.origin,
             "headers": dict(request.headers),
@@ -36,9 +45,22 @@ def bots_handler(path):
         }
         if type(data["request_data"]) is bytes:
             data["request_data"] = data["request_data"].decode()
+        
+        try:
+            if request.files:
+                files_renaming_map = dict()
+                for filename in request.files:
+                    new_filename = str(uuid.uuid4())
+                    files_renaming_map[new_filename] = filename
+                    logger.error(request.files[filename])
+                    request.files[filename].save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
+
+                data["request_data"] = f"<Files Uploaded>: {files_renaming_map}" 
+        except Exception as e:
+            logger.error(f"Failed saving file/s {e}")
 
         client.main_database.bots_collection.insert_one(data).inserted_id
-    except Exeption as e:
+    except Exception as e:
         logger.error(e)
 
     return ""
